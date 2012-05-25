514b6a3fda576d33de224502f26f238d9b4ead15
Reda Lemeden <reda@thoughtbot.com>
2012-05-25 10:51:52 -0400

    Use jQuery Waypoints.
    - A step in the landing page becomes visible when the user scrolls past the previous one.

--- app/assets/javascripts/jquery.welcome.js
+++ app/assets/javascripts/jquery.welcome.js
@@ -1,17 +1,30 @@
 var welcomeHeader = $('#js-welcome-header');
+var steps = $('section.steps li');
 
 $(document).ready(function() {
-   setMarginHeights();
+  setMarginHeights();
+
+  // Displays the first step when the user starts scrolling
+  welcomeHeader.waypoint(function() {
+    $('.steps li:first-child').css('visibility', 'visible');
+  });
+
+  // Shows a step after the user scrolls past the previosu one
+  steps.waypoint(function() {
+    $(this).next('li').css('visibility', 'visible');
+  }, {
+    triggerOnce: 'true'
+  });
+
 });
 
 $(window).resize(function() {
-   setMarginHeights();
+  setMarginHeights();
 });
 
 // Function that sets the top-margin and the height of the steps
 var setMarginHeights = function() {
 
-  var steps = $('section.steps li');
   var height = window.innerHeight;
   var welcomeHeader = $('#js-welcome-header');
   var headerMargin = (window.innerHeight - welcomeHeader.height()) / 2;
@@ -20,5 +33,12 @@ var setMarginHeights = function() {
     welcomeHeader.css('margin-top', headerMargin);
   }
 
-  steps.css('height', headerMargin*0.6);
+  steps.css({
+    'height': headerMargin*0.6,
+    'margin-top': headerMargin*0.2,
+    'margin-bottom': headerMargin*0.2
+  });
+
+  $('section.steps li:last-child').css('margin-bottom', headerMargin);
 };
+
