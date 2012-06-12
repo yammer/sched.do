c5a833821df2e1643d535378a6f10d4d843d01ee
Josh Steiner <josh@jsteiner.me>
2012-06-12 10:20:07 -0400

    Remove unused step definition

--- spec/acceptance/step_definitions/voting_steps.rb
+++ spec/acceptance/step_definitions/voting_steps.rb
@@ -6,10 +6,6 @@ step 'I unvote for :suggestion_description' do |suggestion_description|
   vote_for(suggestion_description)
 end
 
-step 'I vote for :suggestion_description again' do |suggestion_description|
-  vote_for(suggestion_description)
-end
-
 step 'I should see that that :suggestion_description has :number vote(s)' do |suggestion_description, number|
   assert_vote_count(suggestion_description, number)
 end
