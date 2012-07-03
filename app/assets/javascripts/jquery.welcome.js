15a820debc15cbbc7d27884be8ec114e6c7e8b3c
Reda Lemeden <reda@thoughtbot.com>
2012-07-03 18:26:21 +0100

    WIP: Better scroll animations and timing.

--- app/assets/javascripts/jquery.welcome.js
+++ app/assets/javascripts/jquery.welcome.js
@@ -6,9 +6,9 @@ $(document).ready(function() {
   // Displays the first step when the user starts scrolling
   scrollCallout.waypoint(function() {
     $('.steps li:first-child').css('visibility', 'visible').addClass('animated');
-    scrollCallout.hide();
+    scrollCallout.slideUp('fast');
   }, {
-   offset: 250
+   offset: '51%'
   });
 
   // Shows a step after the user scrolls past the previous one
