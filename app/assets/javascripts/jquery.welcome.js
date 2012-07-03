d3fc8a7b2c46905a309cf75e9931955999f06ef9
Reda Lemeden <reda@thoughtbot.com>
2012-07-03 14:25:54 +0100

    New landing page design.

--- app/assets/javascripts/jquery.welcome.js
+++ app/assets/javascripts/jquery.welcome.js
@@ -1,37 +1,21 @@
-var welcomeHeader = $('#js-welcome-header');
+var scrollCallout = $('div.learn-more');
 var steps = $('section.steps li');
 
 $(document).ready(function() {
-  // setMarginHeights();
 
   // Displays the first step when the user starts scrolling
-  welcomeHeader.waypoint(function() {
-    $('.steps li:first-child').css('visibility', 'visible');
+  scrollCallout.waypoint(function() {
+    $('.steps li:first-child').css('visibility', 'visible').addClass('animated');
+    scrollCallout.hide();
+  }, {
+   offset: 250
   });
 
   // Shows a step after the user scrolls past the previous one
   steps.waypoint(function() {
-    // $(this).next('li').css('visibility', 'visible').addClass('animated');
-    $(this).next('li').css('visibility', 'visible');
+    $(this).next('li').css('visibility', 'visible').addClass('animated');
   }, {
     triggerOnce: 'true'
   });
 
 });
-
-$(window).resize(function() {
-  // setMarginHeights();
-});
-
-// Function that sets the top-margin and the height of the steps
-var setMarginHeights = function() {
-
-  var viewportHeight = window.innerHeight;
-  var viewportWidth = window.innerWidth;
-  var headerHeight = viewportHeight * 0.65;
-
-  if (viewportWidth > 480) {
-    welcomeHeader.css('height', headerHeight);
-  }
-};
-
