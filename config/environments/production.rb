8034982fa5798ca5adeed71d259935d902f51145
Josh Steiner <josh@jsteiner.me>
2012-07-06 11:18:32 -0400

    Setup mailing for production. Disable mail errors in development.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ SchedDo::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'sched-do.com' }
+  config.action_mailer.default_url_options = { :host => 'sched.do' }
 end
