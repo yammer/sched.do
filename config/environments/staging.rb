5424407fe82dc4606909cbffe361e28ce1da5507
Jason Draper <jason@drapergeek.com>
2012-07-10 09:21:18 -0400

    do not raise errors on email delivery and use email settings in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -49,7 +49,7 @@ SchedDo::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable threaded mode
   # config.threadsafe!
@@ -66,4 +66,13 @@ SchedDo::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.action_mailer.default_url_options = { :host => 'staging.sched-do.com' }
+  ActionMailer::Base.smtp_settings = {
+    address:        'smtp.sendgrid.net',
+    port:           '587',
+    authentication: :plain,
+    user_name:      ENV['SENDGRID_USERNAME'],
+    password:       ENV['SENDGRID_PASSWORD'],
+    domain:         'heroku.com'
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
