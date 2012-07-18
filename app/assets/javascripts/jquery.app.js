33a9c8471ac36df1edc7883cfb0b45cf48d2ca15
Reda Lemeden <reda@thoughtbot.com>
2012-07-18 13:31:46 +0200

    Remove time picker and unused JS assets.

--- app/assets/javascripts/jquery.app.js
+++ app/assets/javascripts/jquery.app.js
@@ -1,52 +1,12 @@
 $(document).ready(function() {
 
   var datepicker = function(){
-    // Displays a date/time picker when suggetsion field is focused
+    // Displays a date picker when suggetsion field is focused
     $('input[data-role="primary-suggestion"]').datepicker({
       nextText: "▶",
       prevText: "◀",
       dateFormat: 'D, M dd yy',
       constrainInput: false,
-    });
-
-    $('input[data-role="secondary-suggestion"]').timepicker({
-      timeFormat: 'hh:mm TT z',
-      constrainInput: false,
-      ampm: true,
-      stepMinute: 5,
-      hourGrid: 3,
-      minuteGrid: 5,
-      hour: 8,
-      minute: 0,
-      timezone: '',
-      showTimezone: true,
-      timezoneList: [
-        { value: 'UTC-11', label: '(-11h) Pago Pago, Alofi' },
-        { value: 'UTC-10', label: '(-10h) Honolulu, Papeete' },
-        { value: 'UTC-9', label: '(-9h) Anchorage, Juneau' },
-        { value: 'UTC-8', label: '(-8h) San Francisco, Vancouver' },
-        { value: 'UTC-7', label: '(-7h) Denver, Edmonton' },
-        { value: 'UTC-6', label: '(-6h) Chicago, Mexico City' },
-        { value: 'UTC-5', label: '(-5h) New York City, Toronto' },
-        { value: 'UTC-4', label: '(-4h) Santiago de Chile, Halifax' },
-        { value: 'UTC-3', label: '(-3h) Rio de Janeiro, Buenos Aires' },
-        { value: 'UTC-2', label: '(-2h) Fernando de Noronha' },
-        { value: 'UTC-1', label: '(-1h) Praia, Ponta Delgada' },
-        { value: 'UTC', label: '(UTC) London, Lisbon, Casablanca' },
-        { value: '', label: 'Time zone not set' },
-        { value: 'UTC+1', label: '(+1h) Madrid, Paris, Berlin, Rome' },
-        { value: 'UTC+2', label: '(+2h) Helsinki, Athens, Cairo' },
-        { value: 'UTC+3', label: '(+3h) Nairobi, Baghdad, Riyadh' },
-        { value: 'UTC+4', label: '(+4h) Dubai, Moscow' },
-        { value: 'UTC+5', label: '(+5h) Karachi, Lahore' },
-        { value: 'UTC+6', label: '(+6h) Dhaka, Chittagong' },
-        { value: 'UTC+7', label: '(+7h) Jakarta, Bangkok' },
-        { value: 'UTC+8', label: '(+8h) Beijing, Singapore' },
-        { value: 'UTC+9', label: '(+9h) Seoul, Tokyo' },
-        { value: 'UTC+10', label: '(+10h) Sydney, Melbourne' },
-        { value: 'UTC+11', label: '(+11h) Vladivostok, Noumea' },
-        { value: 'UTC+12', label: '(+12h) Auckland, Wellington' },
-        ]
     });
   };
 
