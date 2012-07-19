bfce060f51adbd80ef8cec26c30fc8516d971b14
Adarsh Pandit <adarsh@thoughtbot.com>
2012-07-19 12:22:55 -0700

    [wip] Building FakeYammer endpoint and tests

---
+++ app/models/private_message.rb
@@ -0,0 +1,2 @@
+class PrivateMessage
+end
---
+++ spec/models/private_message_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe PrivateMessage do
+  it 'posts to a Yammer Private Message when inviting users' do
+    event = build_stubbed_event
+
+    RestClient.should have_received(:post)
+  end
+
+  #helper methods
+
+  def build_stubbed_event
+    event = build_stubbed(:event)
+    event.generate_uuid
+    invitees = [build_stubbed(:user), build_stubbed(:user)]
+    event.stubs(:invitees).returns(invitees)
+    event
+  end
+end
