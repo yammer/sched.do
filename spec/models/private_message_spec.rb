d10195fc319154d9bacc435b95b6a1b775515a6d
Mason Fischer <mason@thoughtbot.com>
2012-07-19 14:04:32 -0700

    WIP implementing private message

--- spec/models/private_message_spec.rb
+++ spec/models/private_message_spec.rb
@@ -1,13 +1,28 @@
 require 'spec_helper'
 
 describe PrivateMessage do
-  it 'posts to a Yammer Private Message when inviting users' do
+  it 'posts to a Yammer Private Message when created' do
     event = build_stubbed_event
+    build_private_message({ access_token: 'ABC123',
+                             name: 'Fred Jones',
+                             email: 'fred@example.com',
+                             message: 'You have been invited to the event "Clown party"',
+                          }).create
 
-    RestClient.should have_received(:post)
+    FakeYammer.activity_endpoint_hits.should eq(1)
+
   end
 
-  #helper methods
+  def build_private_message(args)
+    user = build_stubbed(:user,
+                         email: args[:email],
+                         access_token: args[:access_token],
+                         name: args[:name])
+    message = args[:message]
+
+    PrivateMessage.new(user, message, args[:event])
+  end
+
 
   def build_stubbed_event
     event = build_stubbed(:event)
