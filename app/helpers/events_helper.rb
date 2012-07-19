49dc58e126b1f3d3a607d438df48797c80a5ac34
Reda Lemeden <reda@thoughtbot.com>
2012-07-19 18:07:37 +0200

    Change formatting.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,6 +1,7 @@
 module EventsHelper
   def event_invitee_username(event, invitee, &block)
     content = image_tag('/assets/bullhorn.svg') + capture(&block)
+
     if event.user_owner?(invitee)
       content_tag(:td, content, :class => 'event-creator')
     else
