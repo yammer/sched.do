9ec255b0ee1a7e368d434fdd54afc0e337d3bb3a
Mason Fischer <mason@thoughtbot.com>
2012-07-19 14:04:32 -0700

    WIP implementing private message

--- app/models/private_message.rb
+++ app/models/private_message.rb
@@ -1,2 +1,21 @@
 class PrivateMessage
+  MESSAGES_ENDPOINT = "https://www.yammer.com/api/v1/messages.json"
+  def initialize(user, action, event)
+    @action = action
+    @user = user
+    @event = event
+  end
+
+  def create
+    response = RestClient.post MESSAGES_ENDPOINT, { 
+      access_token: @user.access_token,
+      message: @message
+    }
+
+  rescue Exception => e
+    Rails.logger.debug(e.response.inspect)
+    Rails.logger.debug(generate_json.inspect)
+    raise
+  end
+
 end
