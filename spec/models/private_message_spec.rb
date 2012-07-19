df9a71d57000250f3f5cf851d4e2eb1f9d8a77f6
Mason Fischer <mason@thoughtbot.com>
2012-07-19 14:04:32 -0700

    Implementing private message on Invitation.create

--- spec/models/private_message_spec.rb
+++ spec/models/private_message_spec.rb
@@ -2,33 +2,12 @@ require 'spec_helper'
 
 describe PrivateMessage do
   it 'posts to a Yammer Private Message when created' do
-    event = build_stubbed_event
-    build_private_message({ access_token: 'ABC123',
-                             name: 'Fred Jones',
-                             email: 'fred@example.com',
-                             message: 'You have been invited to the event "Clown party"',
-                          }).create
-
-    FakeYammer.activity_endpoint_hits.should eq(1)
-
-  end
-
-  def build_private_message(args)
-    user = build_stubbed(:user,
-                         email: args[:email],
-                         access_token: args[:access_token],
-                         name: args[:name])
-    message = args[:message]
-
-    PrivateMessage.new(user, message, args[:event])
-  end
-
-
-  def build_stubbed_event
     event = build_stubbed(:event)
-    event.generate_uuid
-    invitees = [build_stubbed(:user), build_stubbed(:user)]
-    event.stubs(:invitees).returns(invitees)
-    event
+    recipient = build_stubbed(:user)
+    message = 'You are invited to the "Clown party"'
+
+    private_message = PrivateMessage.new(event, recipient, message).create
+
+    FakeYammer.messages_endpoint_hits.should == 1
   end
 end
