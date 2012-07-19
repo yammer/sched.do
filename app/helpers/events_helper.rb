845add591a7a41382722b0c92111f65829988986
Reda Lemeden <reda@thoughtbot.com>
2012-07-19 14:49:08 +0200

    Move event organizer conditional to helper method.

---
+++ app/helpers/events_helper.rb
@@ -0,0 +1,12 @@
+module EventsHelper
+  def event_invitee_username(event, invitee, &block)
+    content = image_tag('/assets/bullhorn.svg') + capture(&block)
+    if event.user_owner?(invitee)
+      content_tag(:td, content, :class => 'event-creator')
+    else
+      content_tag(:td) do
+        block.call
+      end
+    end
+  end
+end
