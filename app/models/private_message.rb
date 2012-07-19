cea084950fb441111bb27afe0338050a8a9b60ce
Mason Fischer <mason@thoughtbot.com>
2012-07-19 14:04:32 -0700

    WIP implementing private message

--- app/models/private_message.rb
+++ app/models/private_message.rb
@@ -1,23 +1,21 @@
 class PrivateMessage
-  Rails.application.routes.default_url_options = ActionMailer::Base.default_url_options
-  MESSAGES_ENDPOINT = "https://www.yammer.com/api/v1/messages.json"
-
-  def initialize(event, recipient, message)
+ MESSAGES_ENDPOINT = "https://www.yammer.com/api/v1/messages.json"
+  def initialize(user, action, event)
+    @action = action
+    @user = user
     @event = event
-    @user = event.user
-    @recipient = recipient
-    @message = message
   end
 
   def create
-    uri = Addressable::URI.new
-    uri.query_values = {
+    response = RestClient.post MESSAGES_ENDPOINT, { 
       access_token: @user.access_token,
-      body: @message,
-      direct_to_id: @recipient.yammer_user_id,
-      og_url: Rails.application.routes.url_helpers.event_url(@event)
+      message: @message
     }
 
-    response = RestClient.post MESSAGES_ENDPOINT + "?" + uri.query.to_s, nil
+  rescue Exception => e
+    Rails.logger.debug(e.response.inspect)
+    Rails.logger.debug(generate_json.inspect)
+    raise
   end
+
 end
