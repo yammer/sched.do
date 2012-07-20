621af1d60c2c9f809d10ab5fd1bd829cd2cdd41a
Mason Fischer <mason@thoughtbot.com>
2012-07-20 10:32:44 -0700

    Modified field finding step to use placeholder

--- spec/acceptance/step_definitions/form_steps.rb
+++ spec/acceptance/step_definitions/form_steps.rb
@@ -6,6 +6,11 @@ step 'I press :button' do |button|
   click_button button
 end
 
-step "I should see :field filled in with :value" do |field, value|
-  find_field(field).value.should eq value
+step "I should see :field filled in with :value" do |field, expected_value|
+  value = begin
+    find_field(field).value 
+  rescue Capybara::ElementNotFound
+    find("input[@placeholder='#{field}']").value
+  end
+  value.should == expected_value
 end
