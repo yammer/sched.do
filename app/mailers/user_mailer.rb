58b1f34d18e7232b76a561d51dcdc816774989ec
Mason Fischer <mason@thoughtbot.com>
2012-07-24 15:58:18 -0700

    Mailer style changes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,17 +3,16 @@ class UserMailer < ActionMailer::Base
     @user = vote.votable.user
     @event = vote.suggestion.event
     mail(to: @user.email,
-         from: "votes@sched.do",
-         subject: "#{@user.name}, thanks for voting with Sched.do",
-        )
+      from: 'votes@sched.do',
+      subject: "#{@user.name}, thanks for voting with Sched.do")
   end
 
   def invitation(guest, event)
     @guest = guest
     @event = event
     mail(to: @guest.email,
-         from: "invitation@sched.do",
-         subject: "You have been invited to a Sched.do event!")
+      from: 'invitation@sched.do',
+      subject: "You have been invited to a Sched.do event!")
   end
 
 
