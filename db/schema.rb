537b67f99e440449cb8b20dfc86e96d69ac4cfcd
Adarsh Pandit <adarsh@thoughtbot.com>
2012-08-08 16:47:15 -0700

    Remove db/schema.rb from .gitignore

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,22 @@
 
 ActiveRecord::Schema.define(:version => 20120808231312) do
 
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
+
   create_table "events", :force => true do |t|
     t.string   "name",                    :null => false
     t.datetime "created_at",              :null => false
