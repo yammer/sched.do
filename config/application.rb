d6e40dccef1d85b7c8ad6bb7aaa828ec1fb5bb92
Mason Fischer <mason@thoughtbot.com>
2012-08-08 09:31:15 -0700

    Bump assets version

--- config/application.rb
+++ config/application.rb
@@ -63,7 +63,7 @@ module SchedDo
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.1'
+    config.assets.version = '1.2'
 
     config.assets.initialize_on_precompile = false
 
