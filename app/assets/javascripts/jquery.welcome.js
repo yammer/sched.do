d6776b5d603ddb997561de23c3286fb2e3a0cef5
Galen Frechette <galen@thoughtbot.com>
2012-08-17 15:24:13 -0400

    uses jquery waypoints to hide and show learn more tab

--- app/assets/javascripts/jquery.welcome.js
+++ app/assets/javascripts/jquery.welcome.js
@@ -6,18 +6,25 @@ $(document).ready(function() {
   $('.steps li:first-child').css('visibility', 'visible').addClass('animated');
 
   // Shows a step after the user scrolls past the previous one
-  steps.waypoint(function() {
-    $(this).next('li').css('visibility', 'visible').addClass('animated');
+  steps.waypoint(function(event, direction) {
+      if (direction === 'down' && $(this).hasClass('first-step')) {
+        $('a.learn-more-fixed').addClass('fade-out');
+      }
+      else if ($(this).hasClass('first-step')) {
+        $('a.learn-more-fixed').removeClass('fade-out');
+      }
+
+    $(this).css('visibility', 'visible').addClass('animated');
   }, {
-    triggerOnce: 'true',
-    offset: '15%'
+    offset: '100%'
   });
 
   // Smooth scrolls from learn more link to steps
-  $('a.learn-more').click(function(){
+  $('a.learn-more-fixed').click(function(){
       $('html, body').animate({
           scrollTop: $( $(this).attr('href') ).offset().top
       }, 500);
+      $(this).addClass('fade-out');
       return false;
   });
 });
