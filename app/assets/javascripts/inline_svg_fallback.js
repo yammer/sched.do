8b38696dd2ccc8631ecd4c38d98e302553d6fef4
Galen Frechette <galen@thoughtbot.com>
2012-08-29 22:57:43 -0700

    adds stylesheet for png fallbacks, adds pngs of reminaing svg images

--- app/assets/javascripts/inline_svg_fallback.js
+++ app/assets/javascripts/inline_svg_fallback.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  if(Modernizr.svg) {
+  if(!Modernizr.svg) {
     var images = $('img[data-png-fallback]');
     images.each(function(i) {
       $(this).attr('src', $(this).data('png-fallback'));
