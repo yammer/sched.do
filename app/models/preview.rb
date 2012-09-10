0f8347fb322185167b923a82e18921180ec86466
Mason Fischer <mason@thoughtbot.com>
2012-09-10 16:33:40 -0700

    Only define mail_view in test and development

--- app/models/preview.rb
+++ app/models/preview.rb
@@ -1,19 +1,21 @@
-class Preview < MailView
-  def event_created_confirmation(event)
-    event = Event.first
-    UserMailer.event_created_confirmation(event)
-  end
+if Rails.env.development? || Rails.env.test?
+  class Preview < MailView
+    def event_created_confirmation(event)
+      event = Event.first
+      UserMailer.event_created_confirmation(event)
+    end
 
-  def invitation
-    guest = Guest.first
-    event = Event.first
-    UserMailer.invitation(guest, event)
-  end
+    def invitation
+      guest = Guest.first
+      event = Event.first
+      UserMailer.invitation(guest, event)
+    end
 
-  def vote_confirmation
-    vote = Vote.first
-    user = vote.voter
-    event = vote.suggestion.event
-    UserMailer.vote_confirmation(vote)
+    def vote_confirmation
+      vote = Vote.first
+      user = vote.voter
+      event = vote.suggestion.event
+      UserMailer.vote_confirmation(vote)
+    end
   end
 end
