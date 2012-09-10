437cb779f4c71e4df738396ea6e42691b32b40d7
Mason Fischer <mason@thoughtbot.com>
2012-09-10 13:11:26 -0700

    Hide authentication error messages during tests

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 require Rails.root.join('lib', 'yammer-strategy')
 require Rails.root.join('lib', 'yammer-staging-strategy')
 OmniAuth.config.on_failure = SessionsController.action(:oauth_failure)
+OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :yammer, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
