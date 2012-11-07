61567413fe0a72667452608f52542fc64cc0ef3c
Adarsh Pandit <adarsh@thoughtbot.com>
2012-11-07 13:34:19 -0800

    Remove blue bar at top of embedFeed
    * Set embedFeed to be headerless

--- app/assets/javascripts/yammer_feed.js
+++ app/assets/javascripts/yammer_feed.js
@@ -2,6 +2,9 @@ if("access_token" in window && isOGObject()){
   yam.connect.embedFeed({
     container: '#yammer-feed',
     feedType: 'open-graph',
+    config: {
+      header: false
+    },
     network: $('meta[name=network_id]').attr('content'),
     objectProperties: {
       url: $("[property='og:url']").attr('content'),
