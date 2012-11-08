713c6d747e0dabfea8289115f8412401f91ce473
Adarsh Pandit <adarsh@thoughtbot.com>
2012-11-08 10:59:35 -0800

    PrivateMessenger formatting updates
    * Formatting changes made in the process of some research
    * Replace double quotes with single
    * Resort order of methods for maximum readability

--- app/models/private_messenger.rb
+++ app/models/private_messenger.rb
@@ -9,28 +9,28 @@ class PrivateMessenger
     send(@message)
   end
 
-  def reminder
-    @invitation = @message_object
-    @event = @invitation.event
-    @message_body = reminder_message_body
-  end
-
-  def group_reminder
-    @invitation = @message_object
-    @event = @invitation.event
-    @message_body = reminder_message_body
-  end
-
   def invitation
     @invitation = @message_object
     @event = @invitation.event
     @message_body = invitation_message_body
   end
 
+  def reminder
+    @invitation = @message_object
+    @event = @invitation.event
+    @message_body = reminder_message_body
+  end
+
   def group_invitation
     @invitation = @message_object
     @event = @invitation.event
     @message_body = group_invitation_message_body
+  end
+
+  def group_reminder
+    @invitation = @message_object
+    @event = @invitation.event
+    @message_body = reminder_message_body
   end
 
   def deliver
@@ -43,14 +43,30 @@ class PrivateMessenger
 
   private
 
-  def reminder_message_body
-    <<-BODY.strip_heredoc
-      Reminder: Help out #{@event.owner} by voting on "#{@event.name}".
+  def send_user_message
+    post_to_yammer({
+      body: @message_body,
+      direct_to_id: @recipient.yammer_user_id,
+      og_url: event_url(@event)
+    })
+  end
 
-      Please click this link to view the options and vote: #{event_url(@event)}
+  def send_group_message
+    post_to_yammer({
+      body: @message_body,
+      group_id: @recipient.yammer_group_id,
+      og_url: event_url(@event)
+    })
+  end
 
-      *This poll was sent using sched.do. Create your own polls for free at #{root_url}
-    BODY
+  def post_to_yammer(message)
+    RestClient.post messages_endpoint + '?' +
+      message.merge(access_token: @sender.access_token).
+      to_query, nil
+  end
+
+  def messages_endpoint
+    @sender.yammer_endpoint + '/api/v1/messages.json'
   end
 
   def invitation_message_body
@@ -63,6 +79,16 @@ class PrivateMessenger
      BODY
   end
 
+  def reminder_message_body
+    <<-BODY.strip_heredoc
+      Reminder: Help out #{@event.owner} by voting on "#{@event.name}".
+
+      Please click this link to view the options and vote: #{event_url(@event)}
+
+      *This poll was sent using sched.do. Create your own polls for free at #{root_url}
+    BODY
+  end
+
   def group_invitation_message_body
     <<-BODY.strip_heredoc
       Attention #{@recipient.name}: #{@event.owner} created the "#{@event.name}" poll and I want your input.
@@ -70,31 +96,5 @@ class PrivateMessenger
       Please click this link to view the options and vote: #{event_url(@event)}
       *This poll was sent using sched.do. Create your own polls for free at #{root_url}
     BODY
-  end
-
-  def messages_endpoint
-    @sender.yammer_endpoint + '/api/v1/messages.json'
-  end
-
-  def post_to_yammer(message)
-    RestClient.post messages_endpoint + "?" +
-      message.merge(access_token: @sender.access_token).
-      to_query, nil
-  end
-
-  def send_group_message
-    post_to_yammer({
-      body: @message_body,
-      group_id: @recipient.yammer_group_id,
-      og_url: event_url(@event)
-    })
-  end
-
-  def send_user_message
-    post_to_yammer({
-      body: @message_body,
-      direct_to_id: @recipient.yammer_user_id,
-      og_url: event_url(@event)
-    })
   end
 end
--- spec/models/private_messenger_spec.rb
+++ spec/models/private_messenger_spec.rb
@@ -1,64 +1,65 @@
 require 'spec_helper'
 
-describe PrivateMessenger, "#reminder" do
-  it 'sends a reminder' do
-    user = create(:user)
+describe PrivateMessenger, '#invitation' do
+  it 'sends the correct invitation' do
+    event_owner = create(:user)
     invitee = create(:user)
-    message = :reminder
-    event = create(:event, owner: user)
+    message = :invitation
+    event = create(:event, owner: event_owner)
     invitation = build(:invitation, event: event, invitee: invitee)
 
-    PrivateMessenger.new(user, message, event.owner, invitation).deliver
+    PrivateMessenger.new(invitee, message, event_owner, invitation).deliver
 
     FakeYammer.messages_endpoint_hits.should == 1
-    FakeYammer.message.should include("Reminder")
-    FakeYammer.message.should include(user.name)
+    FakeYammer.message.should include('vote')
+    FakeYammer.message.should include(event_owner.name)
   end
 end
 
-describe PrivateMessenger, "#group_reminder" do
-  it 'sends a group reminder' do
-    user = create(:user)
-    group = create(:group)
-    message = :group_reminder
-    event = create(:event, owner: user)
-    invitation = build(:invitation, event: event, invitee: group)
+describe PrivateMessenger, '#reminder' do
+  it 'sends the correct reminder' do
+    event_owner = create(:user)
+    invitee = create(:user)
+    message = :reminder
+    event = create(:event, owner: event_owner)
+    invitation = build(:invitation, event: event, invitee: invitee)
 
-    PrivateMessenger.new(user, message, event.owner, invitation).deliver
+    PrivateMessenger.new(invitee, message, event_owner, invitation).deliver
 
     FakeYammer.messages_endpoint_hits.should == 1
-    FakeYammer.message.should include("Reminder")
-    FakeYammer.message.should include(event.owner.name)
+    FakeYammer.message.should include('Reminder')
+    FakeYammer.message.should include(event_owner.name)
   end
 end
 
-describe PrivateMessenger, "#group_invitation" do
+describe PrivateMessenger, '#group_invitation' do
   it 'sends a group invitation' do
-    user = create(:user)
+    event_owner = create(:user)
     group = create(:group)
     message = :group_invitation
-    event = create(:event, owner: user)
+    event = create(:event, owner: event_owner)
     invitation = build(:invitation, event: event, invitee: group)
 
-    PrivateMessenger.new(group, message, event.owner, invitation).deliver
+    PrivateMessenger.new(group, message, event_owner, invitation).deliver
 
     FakeYammer.messages_endpoint_hits.should == 1
-    FakeYammer.message.should include("I want your input")
+    FakeYammer.message.should include('I want your input')
     FakeYammer.message.should include(group.name)
   end
 end
 
-describe PrivateMessenger, "#invitation" do
-  it 'sends a invitation' do
-    user = create(:user)
-    message = :invitation
-    event = create(:event, owner: user)
-    invitation = build(:invitation, event: event)
+describe PrivateMessenger, '#group_reminder' do
+  it 'sends a group reminder' do
+    event_owner = create(:user)
+    group = create(:group)
+    message = :group_reminder
+    event = create(:event, owner: event_owner)
+    invitation = build(:invitation, event: event, invitee: group)
 
-    PrivateMessenger.new(user, message, event.owner, invitation).deliver
+    PrivateMessenger.new(group, message, event_owner, invitation).deliver
 
     FakeYammer.messages_endpoint_hits.should == 1
-    FakeYammer.message.should include("vote")
-    FakeYammer.message.should include(user.name)
+    FakeYammer.message.should include('Reminder')
+    FakeYammer.message.should include(event_owner.name)
   end
 end
