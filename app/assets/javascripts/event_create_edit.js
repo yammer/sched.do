b8d536119906520dba2ddb77b3c507d55d85c009
Britt Ballard <britt@thoughtbot.com>
2013-02-13 10:13:40 -0800

    Disable submit button on event create form after inital submit
    * Added Javascript to disable the submit button on the new event form
    * This keeps people from submitting the form multiple times accidentally
    
    * https://trello.com/c/8dEWuDuB

--- app/assets/javascripts/event_create_edit.js
+++ app/assets/javascripts/event_create_edit.js
@@ -2,3 +2,9 @@
 //= require events
 //= require jquery.app
 //= require jquery-placeholder
+
+$(document).ready(function(){
+  $('#new_event').submit(function(){
+    $('.cta').prop('disabled', true);
+  });
+});
