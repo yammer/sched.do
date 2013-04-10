a7e68cad9e7aed1b7de27d0278d2ac817ae081af
Jessie  Young <jessie@thoughtbot.com>
2013-04-10 14:19:48 -0700

    Change development action_mailer default url to scheddo.dev
    * localhost:3000 generated different og objects for messages and embed
    * Images were broken on 2nd og object
    
    https://trello.com/c/zl00CEqW

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ SchedDo::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'scheddo.dev' }
 
   config.paperclip_defaults = {
     storage: :s3,
